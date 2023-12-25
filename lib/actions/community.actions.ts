16456e9eea787b37f09345684b5291ad6acd78eb
Your Name <your_email@example.com>
2023-12-25 23:48:29 +0530

    minor changes

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -2,7 +2,7 @@
 
 import { FilterQuery, SortOrder } from "mongoose";
 
-import Community from "./community.actions";
+import Community from "../models/community.model";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 
