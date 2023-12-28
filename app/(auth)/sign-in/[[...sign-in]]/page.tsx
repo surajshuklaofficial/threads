af14c367287cffa55e77a13fadf3933df9259bef
Your Name <your_email@example.com>
2023-12-28 21:29:45 +0530

    redirect after auth bug fixed

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
-  return <div className="w-screen h-screen flex justify-center items-center"><SignIn /></div>;
+  return <div className="w-screen h-screen flex justify-center items-center"><SignIn afterSignInUrl="/onboarding"/></div>;
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
-  return <div className="w-screen h-screen flex justify-center items-center"><SignUp /></div>;
+  return <div className="w-screen h-screen flex justify-center items-center"><SignUp afterSignUpUrl="/onboarding"/></div>;
 }
